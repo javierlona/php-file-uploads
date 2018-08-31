805308faef43c155e78904b59fc9df66430d9899
Javier Lona <javierlona1@gmail.com>
2018-08-30 19:52:00 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+  $max = 5000 * 1024;
+
+  if(isset($_POST['upload'])) {
+    echo '<pre>';
+    print_r($_FILES);
+    echo '</pre>';
+
+    $message = "";
+    $destination = __DIR__ . '/uploaded/';
+
+    if($_FILES['filename']['error'] == 0) {
+      $result = move_uploaded_file($_FILES['filename']['tmp_name'], $destination . $_FILES['filename']['name']);
+      if($result) {
+        $message = $_FILES['filename']['name'] . ' was uploaded successfully.';
+      } else {
+        $message = 'Sorry, there was a problem uploading ' . $_FILES['filename']['name'];
+      }
+    } else {
+      switch ($_FILES['filename']['error']) {
+        case 2:
+          $message = $_FILES['filename']['name'] . ' is too big to upload.';
+          break;
+        case 4:
+          $message = 'No file selected.';
+          break;
+        default:
+          $message = 'Sorry, there was a problem uploading ' .$_FILES['filename']['name'];
+          break;
+      }
+    }
+
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="ie=edge" http-equiv="X-UA-Compatible">
+  <link href="https://bootswatch.com/4/lumen/bootstrap.css" rel="stylesheet">
+  <title>PHP File Uploads</title>
+</head>
+<body>
+  <div class="container">
+    <!-- Forms
+          ================================================== -->
+    <div class="bs-docs-section">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="page-header">
+            <h1 id="forms">Forms</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="bs-component">
+            <?php echo isset($message) ? $message : ''; ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+              <fieldset>
+                <legend>Legend</legend>
+                <div class="form-group">
+                  <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max; ?>">
+                  <label for="exampleInputFile">File input</label> <input aria-describedby=
+                  "fileHelp" class="form-control-file" id="filename" type="file" name="filename">
+                  <small class="form-text text-muted" id="fileHelp">This is some placeholder
+                  block-level help text for the above input. It's a bit lighter and easily wraps to
+                  a new line.</small>
+                </div><button class="btn btn-primary" type="submit" name="upload">Upload File</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</body>
+</html>
