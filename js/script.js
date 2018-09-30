edca1493f5425a93531fdb6bdec8ebf072c4ac2e
Javier Lona <javierlona1@gmail.com>
2018-09-30 17:57:05 -0500

    js gallery portion is complete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ const current = document.querySelector('#current');
 // Returns a nodelist of imgs
 const imgs = document.querySelectorAll('.imgs>img');
 
+
+// Create an event listener to respond to each image clicked
 imgs.forEach(function() {
-  addEventListener('click', (e) => current.src = e.target.src)
+  addEventListener('click', click_image);
 });
+
+// Describe how to handle each image click
+function click_image(event) {
+  // Add fade in class
+  current.classList.add('fade-in');
+
+  // Remove fade-in class after .5 seconds
+  setTimeout(() => current.classList.remove('fade-in'), 500);
+
+  // Change current big image to the selected image src
+  current.src = event.target.src;
+}
